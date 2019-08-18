30fedc68ca0faaaa51c1b89c4e345bf3dc787b17
N3mesix <bryanxd@hotmail.es>
2019-08-18 14:14:18 -0600

    testing update 1

---
+++ javascript/prueba.js
@@ -0,0 +1,7 @@
+let nombre = 'Bryan Peña';
+console.log(nombre);
+const numero = 4.41;
+
+//let d = document.getElementById("unico");
+let d = document.getElementsByClassName("clase")
+console.log(d);
