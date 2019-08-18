a9e7045e9d2a6dce688b741e175b3c1b660602bc
N3mesix <bryanxd@hotmail.es>
2019-08-18 14:44:33 -0600

    testing update 3

--- javascript/prueba.js
+++ javascript/prueba.js
@@ -5,3 +5,7 @@ const numero = 4.41;
 //let d = document.getElementById("unico");
 let d = document.getElementsByClassName("clase")
 console.log(d);
+
+let date = new Date();
+document.body.innerHTML = "<h2>Hola</h2>";
+console.log(date);
